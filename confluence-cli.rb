75c80111e5be2906c24978481462fb93d60c3cff
John Roark <john.roark@envoy.com>
2025-06-06 13:20:55 -0700

    remove sha256 specifier

--- confluence-cli.rb
+++ confluence-cli.rb
@@ -10,7 +10,7 @@ class ConfluenceCli < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/envoy/confluence-cli/releases/download/v1.3.2/confluence-cli_Darwin_x86_64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "sha256:97e764e47ba25ffef6098e5b2d0a0954974c1adcf3ae2906bae96f3693da6860"
+      sha256 "97e764e47ba25ffef6098e5b2d0a0954974c1adcf3ae2906bae96f3693da6860"
 
       def install
         bin.install "confluence"
@@ -18,7 +18,7 @@ class ConfluenceCli < Formula
     end
     if Hardware::CPU.arm?
       url "https://github.com/envoy/confluence-cli/releases/download/v1.3.2/confluence-cli_Darwin_arm64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "sha256:4cb988ec6ed60a8d1584f7020bf81957f790901c7685cdc51a12482a835359c9"
+      sha256 "4cb988ec6ed60a8d1584f7020bf81957f790901c7685cdc51a12482a835359c9"
 
       def install
         bin.install "confluence"
@@ -30,7 +30,7 @@ class ConfluenceCli < Formula
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/envoy/confluence-cli/releases/download/v1.3.2/confluence-cli_Linux_x86_64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-        sha256 "sha256:274eaa7b78f02bc22a66eaeb673068dc0c1d93b1277ec1de59b98a1264671355"
+        sha256 "274eaa7b78f02bc22a66eaeb673068dc0c1d93b1277ec1de59b98a1264671355"
 
         def install
           bin.install "confluence"
