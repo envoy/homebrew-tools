3d57b1d230547a93e547c07eae412588a2d64fde
John Roark <john.roark@envoy.com>
2025-08-11 10:55:19 -0700

    1.3.2 -> 1.4.0

--- confluence-cli.rb
+++ confluence-cli.rb
@@ -5,20 +5,20 @@ require_relative "lib/private_strategy"
 class ConfluenceCli < Formula
   desc "Command line interface for Atlassian Confluence"
   homepage "https://github.com/envoy/confluence-cli"
-  version "1.3.2"
+  version "1.4.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/envoy/confluence-cli/releases/download/v1.3.2/confluence-cli_Darwin_x86_64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "97e764e47ba25ffef6098e5b2d0a0954974c1adcf3ae2906bae96f3693da6860"
+      url "https://github.com/envoy/confluence-cli/releases/download/v1.4.0/confluence-cli_Darwin_x86_64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "46bd8379421d5e20ebf9051674e9b312184894ab9c020ca3e9bb240591c76c96"
 
       def install
         bin.install "confluence"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/envoy/confluence-cli/releases/download/v1.3.2/confluence-cli_Darwin_arm64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "4cb988ec6ed60a8d1584f7020bf81957f790901c7685cdc51a12482a835359c9"
+      url "https://github.com/envoy/confluence-cli/releases/download/v1.4.0/confluence-cli_Darwin_arm64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "9a34778d6c39c62682ed7e14fbc3071305a26a8469b9a684de2cdce34f19d229"
 
       def install
         bin.install "confluence"
@@ -29,8 +29,8 @@ class ConfluenceCli < Formula
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/envoy/confluence-cli/releases/download/v1.3.2/confluence-cli_Linux_x86_64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-        sha256 "274eaa7b78f02bc22a66eaeb673068dc0c1d93b1277ec1de59b98a1264671355"
+        url "https://github.com/envoy/confluence-cli/releases/download/v1.4.0/confluence-cli_Linux_x86_64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
+        sha256 "5d942012eb8e9c024575b4f3f44341b0862aaca0305adee79490ee85aa3c606c"
 
         def install
           bin.install "confluence"
