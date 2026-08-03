46b108f207761a5531677d72b3339bb6d865c1e1
envoy-deploys <envoy-deploys@users.noreply.github.com>
2026-08-03 21:33:40 +0000

    agentluminator: re-apply private download strategy

--- Formula/agentluminator.rb
+++ Formula/agentluminator.rb
@@ -1,24 +1,26 @@
+require_relative "../lib/private_strategy"
+
 class Agentluminator < Formula
   desc "agentluminator canonical Rust binary — hook entry points + operator surface"
   homepage "https://github.com/envoy/agentluminator"
   version "2.8.0"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/envoy/agentluminator/releases/download/v2.8.0/agentluminator-aarch64-apple-darwin.tar.xz"
+      url "https://github.com/envoy/agentluminator/releases/download/v2.8.0/agentluminator-aarch64-apple-darwin.tar.xz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "295719fc60c75816fb3be42e89d6a777ba4976b2a820d0c323a9a4051291897b"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/envoy/agentluminator/releases/download/v2.8.0/agentluminator-x86_64-apple-darwin.tar.xz"
+      url "https://github.com/envoy/agentluminator/releases/download/v2.8.0/agentluminator-x86_64-apple-darwin.tar.xz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "c128757d6ba76d825c72f4a1f786d3aab7ae718332090a0a50ad599a7a8a4a83"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/envoy/agentluminator/releases/download/v2.8.0/agentluminator-aarch64-unknown-linux-gnu.tar.xz"
+      url "https://github.com/envoy/agentluminator/releases/download/v2.8.0/agentluminator-aarch64-unknown-linux-gnu.tar.xz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "03733fc0672d8c2e025a1657d48238170095eef963ad5de3f0279ca5f75138a5"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/envoy/agentluminator/releases/download/v2.8.0/agentluminator-x86_64-unknown-linux-gnu.tar.xz"
+      url "https://github.com/envoy/agentluminator/releases/download/v2.8.0/agentluminator-x86_64-unknown-linux-gnu.tar.xz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "5da8f182252650324e8aac91d626fd31e4b4e6c328b60c87c8c67982b78ea65e"
     end
   end
